d892e5feb1b59a63473b74c9d8f36c79dd8799cf
Daniil <ZDaniil2000@mail.ru>
2022-10-13 10:00:04 +0300

    Project setup
    Just setup a project | Add dependencies, fonts, plugins, colors

--- app/src/main/java/com/example/ecommerce/activities/ShoppingActivity.kt
+++ app/src/main/java/com/example/ecommerce/activities/ShoppingActivity.kt
@@ -1,11 +1,12 @@
-package com.example.ecommerce
+package com.example.ecommerce.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ecommerce.R
 
-class MainActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shopping)
     }
 }
