cd3c20e4c9351e041205d995023c8cb1d100d639
Daniil <ZDaniil2000@mail.ru>
2022-10-13 15:04:09 +0300

    Create introduction fragment
    Create simple design

---
+++ app/src/main/java/com/example/ecommerce/fragments/loginRegister/IntroductionFragment.kt
@@ -0,0 +1,7 @@
+package com.example.ecommerce.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.ecommerce.R
+
+class IntroductionFragment: Fragment(R.layout.fragment_introduction){
+}
---
+++ app/src/main/java/com/example/ecommerce/fragments/loginRegister/RegisterFragment.kt
@@ -0,0 +1,7 @@
+package com.example.ecommerce.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.ecommerce.R
+
+class RegisterFragment: Fragment(R.layout.fragment_register){
+}
